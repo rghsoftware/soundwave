475cc38d68e428b5d07418881ab3963d377ed623
Robert Hamilton <robert@rghsoftware.com>
2025-06-29 11:31:43 -0500

    Initialize mopidy store and service

--- soundwave-web/src/main.ts
+++ soundwave-web/src/main.ts
@@ -2,10 +2,20 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+
+import App from '@/App.vue'
+
+import { useMopidyStore } from '@/stores/mopidy'
+import { MopidyService } from '@/services/mopidy'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
+
+const mopidyStore = useMopidyStore()
+const mopidyService = new MopidyService(mopidyStore)
+mopidyService.connect()
+app.provide('mopidyService', mopidyService)
 
 app.mount('#app')
