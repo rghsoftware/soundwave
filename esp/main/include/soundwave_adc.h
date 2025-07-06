74aa145bf86864ab29976ffdb4ab7c72d7df1c35
Robert Hamilton <robert@rghsoftware.com>
2025-07-06 13:43:54 -0500

    Initial ADC config and read

---
+++ esp/main/include/soundwave_adc.h
@@ -0,0 +1,19 @@
+#ifndef SOUNDWAVE_ADC_H
+#define SOUNDWAVE_ADC_H
+
+#include <esp_adc/adc_cali.h>
+#include <esp_adc/adc_oneshot.h>
+
+typedef struct
+{
+  adc_channel_t channels[2]; // ADC channels to read from
+  adc_oneshot_unit_handle_t handle;
+  int raw_data[2];                      // Raw data from ADC
+  int voltage[2];                       // Converted voltage values
+  adc_cali_handle_t cali_handles[2];    // Calibration handles
+  adc_oneshot_unit_init_cfg_t unit_cfg; // ADC unit configuration
+} soundwave_adc_config_t;
+
+void adc_init(soundwave_adc_config_t *adc_config);
+
+#endif // SOUNDWAVE_ADC_H
--- esp/main/soundwave_main.c
+++ esp/main/soundwave_main.c
@@ -1,8 +1,52 @@
+#include "soundwave_adc.h"
+
+#include <freertos/FreeRTOS.h>
+
+#include <esp_adc/adc_cali.h>
+#include <esp_adc/adc_oneshot.h>
 #include <esp_log.h>
+#include <hal/adc_types.h>
 
 const static char *TAG = "[SOUNDWAVE][MAIN]";
+
+typedef struct
+{
+  soundwave_adc_config_t adc_config; // ADC configuration
+} soundwave_config_t;
 
 void app_main(void)
 {
   ESP_LOGI(TAG, "Starting Soundwave application...");
+  soundwave_config_t soundwave_config = { .adc_config = {
+                                              .channels = { ADC_CHANNEL_0, ADC_CHANNEL_3 },
+                                          } };
+  adc_init(&soundwave_config.adc_config);
+
+  while (1)
+  {
+    adc_oneshot_read(soundwave_config.adc_config.handle,
+        soundwave_config.adc_config.channels[0],
+        &soundwave_config.adc_config.raw_data[0]);
+    adc_cali_raw_to_voltage(soundwave_config.adc_config.cali_handles[0],
+        soundwave_config.adc_config.raw_data[0],
+        &soundwave_config.adc_config.voltage[0]);
+
+    adc_oneshot_read(soundwave_config.adc_config.handle,
+        soundwave_config.adc_config.channels[1],
+        &soundwave_config.adc_config.raw_data[1]);
+    adc_cali_raw_to_voltage(soundwave_config.adc_config.cali_handles[1],
+        soundwave_config.adc_config.raw_data[1],
+        &soundwave_config.adc_config.voltage[1]);
+
+    ESP_LOGI(TAG,
+        "Channel 0: Raw: %d, Voltage: %d mV",
+        soundwave_config.adc_config.raw_data[0],
+        soundwave_config.adc_config.voltage[0]);
+    ESP_LOGI(TAG,
+        "Channel 1: Raw: %d, Voltage: %d mV",
+        soundwave_config.adc_config.raw_data[1],
+        soundwave_config.adc_config.voltage[1]);
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay for 1 second
+  }
 }
