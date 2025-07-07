795a2f97fd29702bbf1ad4681dc9618b3b1acd9a
Robert Hamilton <robert@rghsoftware.com>
2025-07-06 20:31:53 -0500

    Add config schema

--- soundwave-spi/mopidy_soundwave_spi/__init__.py
+++ soundwave-spi/mopidy_soundwave_spi/__init__.py
@@ -4,6 +4,7 @@ import logging
 import pathlib
 
 from mopidy import ext
+from mopidy.config import types as config_types
 
 __version__ = "0.1.0"
 
@@ -16,7 +17,13 @@ class Extension(ext.Extension):
     version = __version__
 
     def get_config_schema(self):
-        return super(Extension, self).get_config_schema()
+        schema = super(Extension, self).get_config_schema()
+        schema["spi_bus"] = config_types.Integer()
+        schema["spi_device"] = config_types.Integer()
+        schema["spi_speed_hz"] = config_types.Integer()
+        schema["spi_mode"] = config_types.Integer()
+
+        return schema
 
     def get_default_config(self):
         config_file = pathlib.Path(__file__).parent / "ext.conf"
